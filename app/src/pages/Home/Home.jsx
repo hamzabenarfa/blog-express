639c4f68dfb8f1d6309f01d24d18982b216a5c0d
hamza benarfa <hamzabenarfa4@gmail.com>
2023-01-15 22:41:45 +0100

    csss

--- app/src/pages/Home/Home.jsx
+++ app/src/pages/Home/Home.jsx
@@ -8,46 +8,42 @@ function Home() {
   return (
     <div>
 
-<div className="main-container">
-    <div className="left-section">
-        <div className="post-section">
-            <div className="post-container">
-                <div className="post-img">
+<div className="amain-container">
+    <div className="aleft-section">
+        <div className="apost-section">
+            
+            <div className="apost-container">
+              
+                <div className="apost-img">
                     <img src="image.jpg" alt="Post 1" />
                 </div>
-                <div className="post-content">
+                <div className="apost-content">
                     <h2>Post 1</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, magna in feugiat congue,
                         ipsum
                         erat convallis ipsum, a malesuada orci ante euismod augue.</p>
                 </div>
-                <div className="post-button">
-                    <a href="#" className="edit-btn">Edit</a>
-                    <a href="#" className="delete-btn">Delete</a>
-                </div>
+            
             </div>
-            <div className="post-container">
-                <div className="post-img">
+            <div className="apost-container">
+                <div className="apost-img">
                     <img src="image.jpg" alt="Post 2" />
                 </div>
-                <div className="post-content">
+                <div className="apost-content">
                     <h2>Post 2</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, magna in feugiat congue,
                         ipsum
                         erat convallis ipsum, a malesuada orci ante euismod augue.</p>
                 </div>
-                <div className="post-button">
-                    <a href="#" className="edit-btn">Edit</a>
-                    <a href="#" className="delete-btn">Delete</a>
-                </div>
+              
             </div>
   
         </div>
     </div>
 
-    <div className="right-section">
-        <div className="category-section">
-            <div className="category-container">
+    <div className="aright-section">
+        <div className="acategory-section">
+            <div className="acategory-container">
                 <h2>Category 1</h2>
                 <ul>
                     <li><a href="#">Subcategory 1</a></li>
