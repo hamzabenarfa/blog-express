7337344b62b1468aeadabdfeaf2e688d7d72007e
Hamza <hamzabenarfa4@gmail.com>
2023-07-01 04:07:42 +0100

    will it work?

--- app/src/index.js
+++ app/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import TagManager from "react-gtm-module";
 
-const tagManagerArgs = {
-  gtmId: "GTM-W3KXZRH",
-};
+// const tagManagerArgs = {
+//   gtmId: "GTM-W3KXZRH",
+// };
 
-TagManager.initialize(tagManagerArgs);
+// TagManager.initialize(tagManagerArgs);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
