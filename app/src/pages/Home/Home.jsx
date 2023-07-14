f459042419c5ab947cd64d28c41ab158f3599de3
Hamza <hamzabenarfa4@gmail.com>
2023-07-14 18:41:28 +0100

    fixed home

--- app/src/pages/Home/Home.jsx
+++ app/src/pages/Home/Home.jsx
@@ -13,29 +13,32 @@ function Home() {
       <section className="flex flex-row justify-around p-4 pl-16  ">
 
         <div>
-          <div className="">
+          <div className="w-96">
             <h1 className="text-2xl font-semibold">Recent blog posts</h1>
             <img src={posts && posts[1].photo} alt="" className="object-cover  w-96" />
+            <div className="flex flex-col">
+              
             <h6 className="text-xs font-semibold capitalize">{posts && posts[1].username} {time(posts && posts[1].createdAt)}</h6>
-            <h1 className="font-bold text-2xl pt-2">{posts && posts[1].title}</h1>
-            <h5 className="text-sm ">{posts && posts[1].descreption}</h5>
+            <h1 className="font-bold text-2xl pt-2">{posts && posts[1].title.substring(0, 30)}</h1>
+            <h5 className="text-sm     ">{posts && posts[1].descreption.substring(0, 200)}</h5>
+            </div>
           </div>
         </div>
 
         <div className="">
           
      { posts && posts.map((post) => ( 
-          <div className=" flex flex-row m-4 p-4  " key={post.id}>
+          <div className=" flex flex-row m-4 p-4 " key={post.id}>
             <div className="">
               <img src={post && post.photo} alt="" className="object-cover w-60" />
             </div>
-            <div className="w-[400px] p-4">
+            <div className="w-[400px] pl-4">
             <h6 className="text-xs font-semibold capitalize">{post && post.username} {time(post && post.createdAt)}</h6>
             <h1 className="font-bold text-2xl pt-2">{post && post.title.substring(0, 30)}...</h1>
             <h5 className="text-sm ">{post && post.descreption.substring(0, 200)}...</h5>
             </div>
           </div>
-      ))
+      )).slice(0, 3)
      }
         </div>
       </section>
