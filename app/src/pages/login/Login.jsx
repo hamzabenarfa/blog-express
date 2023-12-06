461e4e95cc75a213774c133610981a006fcfed31
hamza <hamzabenarfa4@gmail.com>
2023-12-07 00:03:53 +0100

    style: added loding , err to login

--- app/src/pages/login/Login.jsx
+++ app/src/pages/login/Login.jsx
@@ -3,24 +3,32 @@ import "./login.css";
 import { Link, useNavigate } from "react-router-dom";
 import { useRef } from "react";
 import axios from "axios";
-
+import { useState } from "react";
 function Login() {
-  const url = process.env.REACT_APP_URL
+  const url = process.env.REACT_APP_URL;
 
   const history = useNavigate();
   const username = useRef();
   const password = useRef();
 
+  const [error, setError] = useState(false);
+  const [Loading, setLoading] = useState(false);
   async function submit(e) {
     e.preventDefault();
+    setLoading(true);
     try {
       const res = await axios.post(`${url}/auth/login`, {
         username: username.current.value,
         password: password.current.value,
       });
       res.data && history("/profile", { state: { id: res.data._id } });
+
     } catch (err) {
       console.log(err);
+      setError(true);
+      setLoading(false);
+    }finally {
+      setLoading(false);
     }
   }
 
@@ -31,7 +39,20 @@ function Login() {
           <form className="login-form" onSubmit={submit}>
             <input type="text" ref={username} placeholder="username" />
             <input type="password" ref={password} placeholder="password" />
-            <button type="submit" className=" rounded-md">login</button>
+            {error && (
+              <p className="-mt-1 mb-2 text-red-500 text-sm font-bold text-left">
+                Something went wrong!
+              </p>
+            )}
+            {Loading && (
+              <p className="-mt-1 mb-2 text-green-500 text-sm font-bold text-left">
+                Loading...
+              </p>
+            )}
+<button type="submit" disabled={Loading} className={`rounded-md ${Loading && 'opacity-50 cursor-not-allowed'}`}>
+              login
+            </button>
+            
             <p className="message">
               Not registered? <Link to="/register">Create an account</Link>
             </p>
